10defde389089a2feb6cad570a76f185bb1a8e04
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2023-03-08 11:13:09 +0530

    Add files via upload
    This project i created using html,css and javascript

---
+++ sample/index.js
@@ -0,0 +1,48 @@
+const temperateFeild=document.querySelector(".weather1");
+const cityFeild=document.querySelector(".weather2 p");
+const dateFeild=document.querySelector("#date");
+const emojiFeild=document.querySelector("#emoji");
+const weatherFeild=document.querySelector("#sit");
+const searchFeild=document.querySelector("#place");
+const form=document.querySelector("form");
+
+
+form.addEventListener("submit",search);
+
+let target="Saharanpur";
+
+const fetchData=async (target) => {
+    try{
+    const api=`https://api.openweathermap.org/data/2.5/weather?q=${target}&appid=2cd28bb8fb0edeb6c8211e3697b9f5d3&units=metric`;
+    console.log(api);
+    const res=await fetch(api);
+    const aamri=await res.json();
+    console.log(aamri);
+    const {
+        main:{temp},weather:{0:{id,main,desciption,icon}}}=aamri;
+        update(temp,aamri.name ,icon,main,aamri.dt,aamri.timezone);
+} catch(error){
+    alert("Location not found");
+}
+};
+function update(temperate,city,icon,main,dt,tz){
+    temperateFeild.innerHTML=Math.round(temperate)+"°C";
+    // temperateFeild.innerHTML=Math.round((((( temperate - 273.15) * 9/5) + 32)-32)/1.8).toFixed()+"°C";
+    cityFeild.innerHTML=city;
+    emojiFeild.src="http://openweathermap.org/img/w/"+icon+".png"; 
+    weatherFeild.innerHTML=main;
+    dateFeild.innerHTML=new Date().toDateString();
+    // dateFeild.innerHTML=new Date(dt*1000-(tz*1000)).toDateString();
+}
+
+fetchData(target);
+
+function search(e){
+    e.preventDefault();
+    target=searchFeild.value;
+    fetchData(target);
+}
+// const search=(val)=>{
+//     var target=val;
+//     console.log(target);
+// fetchData(target);
